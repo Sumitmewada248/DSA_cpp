95421ac8425c825a99ed90cdb955d41db6cae899
Sumitmewada248 <sumitmewa@gmail.com>
2024-12-26 11:38:21 +0530

    update_bubble

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 void bubble(int arr[],int l)
 {
-cout<<"bubble sort";
+    int c;
+for(int i=0;i<l;i++){
+for(int j=0;j<l-i-1;j++){
+c++;
+if(arr[j]>arr[j+1]){
+int tmp;
+tmp=arr[j+1];
+arr[j+1]
+        }
+    }
+}
 }
 int main(){
 
+
+//Driver Code
 int r;
 cout<<"enter size of array\n";
 cin>>r;
@@ -17,12 +29,10 @@ cout<<"enter the  ranges\n";
     for(int i=0;i<r;i++)
     {
         cin>>arr[i];
-
     }
     bubble(arr,r);
 }
 else{
     cout<<"not valid range";
 }
-
 }
