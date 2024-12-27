36cb6a66a1df7caf7a73d8b8a7baf107b7de9852
Sumitmewada248 <sumitmewa@gmail.com>
2024-12-27 12:28:23 +0530

    insertion

---
+++ Insertion_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void    insrt(int arr[],int s){
+    for(int i=1;i<s;i++){
+        int ram=i;
+        while(ram>0 && arr[ram-1]>arr[ram]){
+            int tmp=arr[ram-1];
+            arr[ram-1]=arr[ram];
+            arr[ram]=tmp;
+            ram--;
+}
+    }
+}
+
+int main(){
+    int arr[]={3,1,4,2,6};
+     int s=sizeof(arr)/sizeof(arr[0]);
+    for (int i=0;i<s;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+        cout<<"after sorting"<<endl;
+
+    insrt(arr,s);
+    for (int i=0;i<s;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+
+
+}
