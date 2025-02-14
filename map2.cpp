c448b8bc869c00b66021dcc065ccb139de221148
Sumitmewada248 <sumitmewa@gmail.com>
2025-02-14 12:01:18 +0530

    map

--- map2.cpp
+++ map2.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 using namespace std;
+#include<map>
 
 int main(){
-    
+map<string,int>mp;
+string name;
+int mob,n;
+
+cout<<"enter how many recodes : ";
+cin>>n;
+
+for(int i=1;i<=n;i++){
+    cout<<"enter name"<<endl;
+    cin>>name;
+    cout<<"enter mobile number"<<endl;
+    cin>>mob;
+    mp.insert({name,mob});
+}
+cout<<"\n results \n";
+for (auto p:mp){
+    cout<<p.first<<","<<p.second<<endl;
+}
 }
