d943b7ec9291521d1fd4a55630ba62fb9a7fa8f7
Sumitmewada248 <sumitmewa@gmail.com>
2025-02-18 11:42:50 +0530

    map

--- Map_multi.cpp
+++ Map_multi.cpp
@@ -5,19 +5,42 @@ using namespace std;
 #include<map>
 #include<vector>
 #include<string>
+#include<set>
+#include<unordered_set>
 
-///Duplicate values are allowed and soting also maintain
-int main(){
-    multimap<int,string>up;
-    up.insert({10,"raju"});
-    up.insert({484,"leela"});
-    up.insert({158,"manhoosh"});
-    up.insert({158,"manhoosh"});
-    up.insert({158,"manhoosh"});
 
-    for (auto p:up){
-        cout<<p.first<<" ";
-        cout<<p.second;
-        cout<<endl;        
-    }
+
+// ///Duplicate values are allowed and soting also maintain
+// int main(){
+//     multimap<int,string>up;
+//     up.insert({10,"raju"});
+//     up.insert({484,"leela"});
+//     up.insert({158,"manhoosh"});
+//     up.insert({158,"manhoosh"});
+//     up.insert({158,"manhoosh"});
+
+//     for (auto p:up){
+//         cout<<p.first<<" ";
+//         cout<<p.second;
+//         cout<<endl;        
+//     }
+// }
+
+//Duplicate values are not allowed and soting also maintain
+// It Work on Single Value only(int ,string)
+// there are three type of set 1(ORDER SET) 2 (UNORDER SET)  3(MULTI SET)
+
+
+
+ int main(){
+   multiset<int>p;
+   p.insert(10);
+   p.insert(484);
+   p.insert(158);
+   p.insert(158);
+   p.insert(2);
+
+   for (auto p:p){
+       cout<<p<<" ";
+   } 
 }
